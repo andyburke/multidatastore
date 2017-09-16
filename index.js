095799a20e7c54451db455fefc3c7f1ac9d55832
Andy Burke <aburke@bitflood.org>
2017-09-15 21:22:17 -0700

    allow passing options

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ const Multi_Data_Store = {
         }
     },
 
-    put: async function( object ) {
+    put: async function( object, options ) {
         const drivers = this._drivers || [];
         for ( let driver of drivers ) {
-            await driver.put( object );
+            await driver.put( object, options );
         }
     },
 
-    get: async function( id ) {
+    get: async function( id, options ) {
         const drivers = this._drivers || [];
         const readable_driver = drivers.find( driver => {
             return driver && driver.options && driver.options.readable;
@@ -55,10 +55,10 @@ const Multi_Data_Store = {
             throw new Error( 'missing readable driver' );
         }
 
-        return await readable_driver.get( id );
+        return await readable_driver.get( id, options );
     },
 
-    find: async function( criteria ) {
+    find: async function( criteria, options ) {
         const drivers = this._drivers || [];
         const searchable_driver = drivers.find( driver => {
             return driver && driver.options && typeof driver.options.find === 'function';
@@ -68,17 +68,17 @@ const Multi_Data_Store = {
             throw new Error( 'missing searchable driver' );
         }
 
-        return await searchable_driver.options.find( criteria, searchable_driver );
+        return await searchable_driver.options.find( criteria, options, searchable_driver );
     },
 
-    del: async function( id  ) {
+    del: async function( id, options ) {
         const drivers = this._drivers || [];
         for ( let driver of drivers ) {
             if ( driver.options && driver.options.ignore_delete ) {
                 continue;
             }
 
-            await driver.del( id );
+            await driver.del( id, options );
         }
     }
 };
